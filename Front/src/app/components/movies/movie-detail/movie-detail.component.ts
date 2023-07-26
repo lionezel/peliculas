429dc147216c9a5afd482da65f4a6bc4a2c65b0c
lionezel <lionezelfts@gmail.com>
2023-07-26 13:49:33 -0500

    creacion del detalles de peliculas

--- Front/src/app/components/movies/movie-detail/movie-detail.component.ts
+++ Front/src/app/components/movies/movie-detail/movie-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { combineLatest } from 'rxjs';
 import { MovieServicesService } from 'src/app/services/movie-services.service';
@@ -8,19 +8,23 @@ import { MovieServicesService } from 'src/app/services/movie-services.service';
   templateUrl: './movie-detail.component.html',
   styleUrls: ['./movie-detail.component.css']
 })
-export class MovieDetailComponent {
+export class MovieDetailComponent implements OnInit{
   public detail_arr: any
+
 
   
 
   constructor(private _movieServices: MovieServicesService, private activatedRoute: ActivatedRoute,) {
     const {id} = this.activatedRoute.snapshot.params
-    this._movieServices.detail_movie(id).subscribe(([movie]) => {
-      console.log(movie)
-      this.detail_arr =  movie;
+    this._movieServices.detail_movie(id).subscribe((response) => {
+      console.log(response)
+      this.detail_arr =  response;
     });
-
   }
+
+ngOnInit(): void {
+
+}
 
   
 }
