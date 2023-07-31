40b0aa20a72044536f3534af4f1045aee89595ac
lionezel <lionezelfts@gmail.com>
2023-07-31 16:06:16 -0500

    login

--- Front/src/app/components/login/login.component.ts
+++ Front/src/app/components/login/login.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  email: any;
+  password: any;
 
+  constructor() {}
+
+  login() {
+    console.log(this.email);
+    console.log(this.password);
+  }
 }
