c05db448034cffb0adabb0e5219b706d95eb87fc
lionezel <lionezelfts@gmail.com>
2023-07-31 16:07:37 -0500

    register

--- Front/src/app/components/register/register.component.ts
+++ Front/src/app/components/register/register.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  email: any;
+  password: any;
+  confirmPassword: any;
 
+  constructor() {}
+
+  register() {
+    console.log(this.email);
+    console.log(this.password);
+  }
 }
