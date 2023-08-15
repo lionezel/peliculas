13227c270605edfa96a7acadb02121a1e61b3643
lionezel <lionezelfts@gmail.com>
2023-08-15 18:23:21 -0500

    people-module

--- Front/src/app/page/page.module.ts
+++ Front/src/app/page/page.module.ts
@@ -9,6 +9,7 @@ import { SerieModule } from './serie/serie.module';
 import { SharedModule } from '../shared/shared.module';
 
 import { PipeModule } from '../pipe/pipe.module';
+import { PeopleModule } from './people/people.module';
 
 @NgModule({
   declarations: [SearchPageComponent],
@@ -19,6 +20,7 @@ import { PipeModule } from '../pipe/pipe.module';
     SerieModule,
     SharedModule,
     PipeModule,
+    PeopleModule
   ],
   exports: [HomeModule, MovieModule, SerieModule, SearchPageComponent],
 })
