c2f3a8f5ee08aaf4ba258e44cd72d5c97b8626d6
lionezel <lionezelfts@gmail.com>
2023-08-15 18:47:39 -0500

    people-list

--- Front/src/app/page/people/people.module.ts
+++ Front/src/app/page/people/people.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PeopleListComponent } from './people-list/people-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PeopleListComponent
+  ],
   imports: [
     CommonModule
   ]
