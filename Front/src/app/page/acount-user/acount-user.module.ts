5a505b5e4a5ad493be1e2e5239f484b4dfd3cf7c
lionezel <lionezelfts@gmail.com>
2023-08-16 07:37:57 -0500

    page/login

--- Front/src/app/page/acount-user/acount-user.module.ts
+++ Front/src/app/page/acount-user/acount-user.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
 })
 export class AcountUserModule {}
--- Front/src/app/page/acount-user/login/login.component.ts
+++ Front/src/app/page/acount-user/login/login.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  public email: string = '';
+  public password: string = '';
 
+  constructor() {}
+
+  login() {
+    console.log(this.email);
+    console.log(this.password);
+  }
 }
