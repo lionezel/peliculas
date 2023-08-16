2f8f466a2ca43f23b584dafc46d23409f4d26499
lionezel <lionezelfts@gmail.com>
2023-08-16 08:05:10 -0500

    servicio que devulve el usuario

--- Front/src/app/services/user.service.ts
+++ Front/src/app/services/user.service.ts
@@ -20,8 +20,15 @@ export class UserService {
   setToken(token: string) {
     this._cookies.set('token', token);
   }
-  
+
   getToken() {
     return this._cookies.get('token');
+  }
+
+  getUser() {
+    return this._http.get("https://reqres.in/api/users/2");
+  }
+  getUserLogged() {
+    const token = this.getToken();
   }
 }
