8b03b192f0371a909880852ecb5c7c74fd4075c4
lionezel <lionezelfts@gmail.com>
2023-08-16 07:48:04 -0500

    mandando el servicio al componente de login

--- Front/src/app/page/acount-user/login/login.component.ts
+++ Front/src/app/page/acount-user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -9,10 +10,12 @@ export class LoginComponent {
   public email: string = '';
   public password: string = '';
 
-  constructor() {}
+  constructor(private _userServices: UserService) {}
 
   login() {
-    console.log(this.email);
-    console.log(this.password);
+    const user = { email: this.email, password: this.password };
+    this._userServices.login(user).subscribe((data) => {
+      console.log(data);
+    });
   }
 }
